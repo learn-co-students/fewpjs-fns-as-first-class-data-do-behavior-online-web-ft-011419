b50b9e174c73ef057c812e5fd79a116eaafce519
Simcha Greenbaum <gbs4ever2@gmail.com>
2019-12-24 20:58:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 /* Given Code, don't edit */
 
 function handleClick(e) {
+
   const timeString = document.getElementById('time').value
   displayMessage(greet(timeString))
+
 }
 
 /* End Given Code, don't edit above here...*/
